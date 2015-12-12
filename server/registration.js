d98769a8cec67c5171014a4fdee0f8dd8e6fe06c
Teruko <elvesstolemybike@gmail.com>
2015-12-12 00:13:19 -0500

    Registration does not require a key if it's the very first user.

--- server/registration.js
+++ server/registration.js
@@ -1,4 +1,8 @@
 Accounts.validateNewUser(function (user) {
+  if (! Meteor.users.findOne()) {
+    // Always allow registration if this is the first user.
+    return true;
+  }
   if (Invites.findOne({ key: user.profile.key })) {
     Invites.remove({ key: user.profile.key });
     return true;

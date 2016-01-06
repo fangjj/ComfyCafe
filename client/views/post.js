69c39ba78aa12eb09a8e33f7f95981dadad50d51
Teruko <elvesstolemybike@gmail.com>
2016-01-05 23:03:12 -0500

    Use with context in post template to make things complain less upon post deletion.

--- client/views/post.js
+++ client/views/post.js
@@ -14,25 +14,25 @@ Template.post.helpers({
     return this.contentType.split("/")[0] === "video";
   },
   isOwner: function () {
-    return Meteor.userId() === this.post.uploader;
+    return Meteor.userId() === this.uploader;
   },
   favorited: function () {
-    return _.contains(this.post.favorited, Meteor.userId());
+    return _.contains(this.favorited, Meteor.userId());
   }
 });
 
 Template.post.events({
   "click #fabFavorite": function (event, template) {
-    Meteor.call("favoritePost", this.post._id, ! _.contains(this.post.favorited, Meteor.userId()));
+    Meteor.call("favoritePost", this._id, ! _.contains(this.favorited, Meteor.userId()));
   },
   "click #fabReroll": function (event, template) {
-    Meteor.call("rerollPost", this.post._id, function (err, name) {
+    Meteor.call("rerollPost", this._id, function (err, name) {
       Router.go("post.view", {name: name});
     });
   },
   "click #fabDelete": function (event, template) {
     var self = this;
-    Meteor.call("deletePost", this.post._id, function () {
+    Meteor.call("deletePost", this._id, function () {
       Router.go("home");
     });
   }

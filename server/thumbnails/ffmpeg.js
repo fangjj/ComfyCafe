29f1564f1b18cf017168192500ab94f037cc6559
Teruko <elvesstolemybike@gmail.com>
2016-01-12 01:27:53 -0500

    Cleaned up ffmpeg backend.

--- server/thumbnails/ffmpeg.js
+++ server/thumbnails/ffmpeg.js
@@ -3,36 +3,44 @@ var fs = Meteor.npmRequire("fs");
 var tmp = Meteor.npmRequire("tmp");
 
 getVideoPreview = function (job, inStream, outStream, callback) {
-  job.progress(80, 100);
-
   var tmpFile = tmp.fileSync();
   var wstream = fs.createWriteStream(tmpFile.name);
+
   inStream.pipe(wstream);
+
   wstream.on("finish", Meteor.bindEnvironment(function () {
+    job.progress(40, 100);
+
     var tmpThumbFile = tmp.fileSync({ postfix: ".png" });
 
     exec("ffmpeg -y -i " + tmpFile.name + " -frames:v 1 " + tmpThumbFile.name,
     Meteor.bindEnvironment(function(err) {
+      job.progress(80, 100);
+
       var rstream = fs.createReadStream(tmpThumbFile.name);
       rstream.pipe(outStream);
-
-      media.update(
-        { _id: job.data.inputFileId },
-        { $set: { "metadata.thumbComplete": true } }
-      );
-
-      job.log("Finished work on thumbnail image: " + (job.data.outputFileId.toHexString()), {
-        level: "info",
-        data: {
-          input: job.data.inputFileId,
-          output: job.data.outputFileId
-        },
-        echo: true
-      });
-
-      job.done();
     }));
+  }));
 
-    return callback();
+  outStream.on("finish", Meteor.bindEnvironment(function () {
+    job.progress(90, 100);
+
+    media.update(
+      { _id: job.data.inputFileId },
+      { $set: { "metadata.thumbComplete": true } }
+    );
+
+    job.log("Finished work on thumbnail image: " + (job.data.outputFileId.toHexString()), {
+      level: "info",
+      data: {
+        input: job.data.inputFileId,
+        output: job.data.outputFileId
+      },
+      echo: true
+    });
+
+    job.done();
+
+    callback();
   }));
 };

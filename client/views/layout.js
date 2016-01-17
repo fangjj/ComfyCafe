fc03ba9aee5850983ae027d79289e4b6e4d5510a
Teruko <elvesstolemybike@gmail.com>
2016-01-16 21:51:59 -0500

    Prevent title from being set to undefined during small gaps between session var setting.

--- client/views/layout.js
+++ client/views/layout.js
@@ -1,5 +1,5 @@
 Deps.autorun(function () {
-  document.title = Session.get("pageTitle");
+  document.title = Session.get("pageTitle") || "TeruImages";
   Meteor.subscribe("media", Meteor.userId());
   Meteor.subscribe("jobs", Meteor.userId());
   $.cookie("X-Auth-Token", Accounts._storedLoginToken());

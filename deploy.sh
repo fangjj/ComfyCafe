ce1c53b60e70cd179860f74cadd4a6b2568d5800
Teruko <elvesstolemybike@gmail.com>
2016-01-16 23:17:32 -0500

    rsyncing to bundle/bundle makes you look silly.

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 set -e
 mkdir -p ../meteor-bundles/TeruImages
 meteor build --directory ../meteor-bundles/TeruImages
-rsync -azPAXvH -e 'ssh -p 222 -i ~/.ssh/id_rsa' --delete ../meteor-bundles/TeruImages/bundle root@git.teru.sexy:/srv/TeruImages/bundle
+rsync -azPAXvH -e 'ssh -p 222 -i ~/.ssh/id_rsa' --delete ../meteor-bundles/TeruImages/bundle root@git.teru.sexy:/srv/TeruImages
 ansible-playbook ansible/site.yml

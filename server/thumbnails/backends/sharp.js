b6b8ee6dfcb89d005dce8aeae26c80f5661c46c4
Teruko <elvesstolemybike@gmail.com>
2016-01-18 03:16:58 -0500

    Always use PNG for Sharp output, since #1 it doesn't support GIF/etc. output, and #2 the only reason animated GIF output was working well before was because it wasn't actually being thumbnailed.

--- server/thumbnails/backends/sharp.js
+++ server/thumbnails/backends/sharp.js
@@ -1,6 +1,6 @@
 var sharp = Meteor.npmRequire("sharp");
 
 sharpImageResize = function (inStream, outStream, width, height) {
-  var thumb = sharp().resize(width, height).max();
+  var thumb = sharp().resize(width, height).max().toFormat("png");
   inStream.pipe(thumb).pipe(outStream);
 };

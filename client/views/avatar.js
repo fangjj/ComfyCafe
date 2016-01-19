933dca6ce4b060908713a4f71968bd7a9d479b09
Teruko <elvesstolemybike@gmail.com>
2016-01-19 07:22:17 -0500

    Added djenticons.

--- client/views/avatar.js
+++ client/views/avatar.js
@@ -1,3 +1,13 @@
+Template.avatar.onRendered(function () {
+	if (! _.has(this.data.profile, "avatar")) {
+		var size = thumbnailPolicies.avatar[this.data.class].size;
+		this.$(".djentCanvas").attr({
+			width: size[0],
+			height: size[1]
+		}).jdenticon("78eed9f7b95f7d7c33432ac5aaba1863")
+	}
+});
+
 Template.avatar.helpers({
 	hasAvatar: function () {
 		return _.has(this.profile, "avatar");

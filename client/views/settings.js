5642b108eefc395e71d04d99e30e9ad00314979f
Teruko <elvesstolemybike@gmail.com>
2016-01-21 08:04:58 -0500

    Improved settings page to show current state and redirect after submit/cancel.

--- client/views/settings.js
+++ client/views/settings.js
@@ -1,7 +1,20 @@
+Template.settings.helpers({
+	privateByDefaultChecked: function () {
+    if (! Meteor.user().profile.privateByDefault) {
+      return "checked";
+    }
+	}
+});
+
 Template.settings.events({
 	"click .submit": function (event, template) {
 		Meteor.call("applySettings", {
       privateByDefault: ! template.$("input[name=privateByDefault]").is(":checked")
+    }, function () {
+      Router.go("user.view", {username: Meteor.user().username});
     });
+	},
+	"click .cancel": function (event, template) {
+		Router.go("user.view", {username: Meteor.user().username});
 	}
 });

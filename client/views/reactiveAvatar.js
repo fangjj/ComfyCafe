9a0969926864d2c98e10b2c4cf2a5e601d9767b0
Teruko <elvesstolemybike@gmail.com>
2016-02-02 18:22:59 -0500

    Completely removed client-side djenticon generation.

--- client/views/reactiveAvatar.js
+++ client/views/reactiveAvatar.js
@@ -1,11 +1,3 @@
-Template.reactiveAvatar.onRendered(function () {
-	var size = thumbnailPolicies.avatar[this.data.class].size;
-	this.$(".djentCanvas").attr({
-		width: size[0],
-		height: size[1]
-	}).jdenticon(this.data.profile.sassyHash);
-});
-
 Template.reactiveAvatar.helpers({
 	hasAvatars: function () {
 		return _.has(this.profile, "avatars");

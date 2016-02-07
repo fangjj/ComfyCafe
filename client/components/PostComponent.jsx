e0f9ec0bc0851fe6d165207320de44d12bd46608
Teruko <elvesstolemybike@gmail.com>
2016-02-07 05:19:58 -0500

    Most complete PostComponent; still needs tags and better markup.

---
+++ client/components/PostComponent.jsx
@@ -0,0 +1,45 @@
+PostComponent = React.createClass({
+  mixins: [ReactMeteorData],
+  getMeteorData() {
+    var handle = Meteor.subscribe("post", FlowRouter.getParam("postId"));
+    return {
+      loading: ! handle.ready(),
+      post: Posts.findOne({ _id: FlowRouter.getParam("postId") }),
+      currentUser: Meteor.user()
+    };
+  },
+  render() {
+    if (this.data.loading) {
+      return <LoadingSpinnerComponent />;
+    }
+
+    var isOwner = this.data.post.uploader
+      && this.data.currentUser._id === this.data.post.uploader._id;
+    var showEditButton = isOwner;
+    var showFavoriteButton = ! isOwner && this.data.currentUser && this.data.post.medium;
+
+    var medium;
+    if (this.data.post) {
+      medium = <MediumComponent medium={this.data.post.medium} />;
+    } else {
+      medium = <Inline404Component />;
+    }
+
+    var fab;
+    if (showEditButton) {
+      fab = <PostModifyFAB post={this.data.post} />;
+    }
+    if (showFavoriteButton) {
+      fab = <FavoriteFAB post={this.data.post} userId={this.data.currentUser._id} />;
+    }
+
+    return <article>
+      <figure className="medium">
+        {medium}
+      </figure>
+      <PostInfoBoxComponent post={this.data.post} />
+      <div className="tags"></div>
+      {fab}
+    </article>;
+  }
+});

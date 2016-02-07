938c0dd0ed8fd33fb0debd37e14977ce6752b960
Teruko <elvesstolemybike@gmail.com>
2016-02-07 05:30:42 -0500

    Made TopBarComponent hopefully less inclined to disappear.

--- client/components/AccountActionsButton.jsx
+++ client/components/AccountActionsButton.jsx
@@ -1,8 +1,14 @@
 AccountActionsButton = React.createClass({
+  mixins: [ReactMeteorData],
+  getMeteorData() {
+    return {
+      currentUser: Meteor.user()
+    };
+  },
   toggleActionsVisbility(event) {
     if (event.button === 1) {
       // Middle mouse click
-      var path = FlowRouter.path("profile", {username: Meteor.user().username});
+      var path = FlowRouter.path("profile", {username: this.data.currentUser.username});
       window.open(path);
     } else {
       event.preventDefault();
@@ -14,9 +20,9 @@ AccountActionsButton = React.createClass({
     return <a id="accountActionsToggle" className="waves-effect waves-teal" onClick={this.toggleActionsVisbility}>
       <ReactiveAvatarComponent
         class="topBar"
-        id={Meteor.userId()}
-        avatars={Meteor.user().avatars}
-        title={Meteor.user().username}
+        id={this.data.currentUser._id}
+        avatars={this.data.currentUser.avatars}
+        title={this.data.currentUser.username}
       />
     </a>;
   }

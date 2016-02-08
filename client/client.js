ea2062cbe2328a2448f24f57dc491109d85ab95f
Teruko <elvesstolemybike@gmail.com>
2016-02-08 02:41:18 -0500

    (Mostly) removed Blaze layout template, which also allowed for the removal of react-template-helper.

---
+++ client/client.js
@@ -0,0 +1,6 @@
+Tracker.autorun(function () {
+  document.title = Session.get("pageTitle") || "TeruImages";
+  Meteor.subscribe("media", Meteor.userId());
+  Meteor.subscribe("jobs", Meteor.userId());
+  $.cookie("X-Auth-Token", Accounts._storedLoginToken());
+});

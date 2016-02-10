e9b8a398260955c2b51bc8a39ced6aac2de3200f
Teruko <elvesstolemybike@gmail.com>
2016-02-10 06:28:09 -0500

    Solved account action button problems thanks to an awesome package.

--- client/components/User/AccountActionsButton.jsx
+++ client/components/User/AccountActionsButton.jsx
@@ -1,25 +1,27 @@
 AccountActionsButton = React.createClass({
   toggleActionsVisbility(event) {
-    if (event.button === 1) {
+    if (event.which === 2) {
       // Middle mouse click
     } else {
-      event.nativeEvent.preventDefault();
-      event.nativeEvent.stopPropagation();
+      event.preventDefault();
+      event.stopPropagation();
       this.props.action();
     }
   },
   render() {
-    return <a id="accountActionsToggle"
-      className="ignore-react-onclickoutside waves-effect waves-teal"
-      href={FlowRouter.path("profile", {username: this.props.currentUser.username})}
-      onClick={this.toggleActionsVisbility}
-    >
-      <ReactiveAvatarComponent
-        class="topBar"
-        id={this.props.currentUser._id}
-        avatars={this.props.currentUser.avatars}
-        title={this.props.currentUser.username}
-      />
-    </a>;
+
+    return <NativeListener onClick={this.toggleActionsVisbility}>
+      <a id="accountActionsToggle"
+        className="ignore-react-onclickoutside waves-effect waves-teal"
+        href={FlowRouter.path("profile", {username: this.props.currentUser.username})}
+      >
+        <ReactiveAvatarComponent
+          class="topBar"
+          id={this.props.currentUser._id}
+          avatars={this.props.currentUser.avatars}
+          title={this.props.currentUser.username}
+        />
+      </a>
+    </NativeListener>;
   }
 });
--- client/lib/client.browserify.js
+++ client/lib/client.browserify.js
@@ -1,3 +1,4 @@
 Cropper = require("cropperjs");
 GeoPattern = require("geopattern");
 tinycolor = require("tinycolor2");
+NativeListener = require("react-native-listener");

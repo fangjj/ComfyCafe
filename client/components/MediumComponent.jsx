20fdd4ce7e7a7de32e2519efd71e33a77203ef26
Teruko <elvesstolemybike@gmail.com>
2016-02-12 16:32:25 -0500

    Medium should always be fully visible in viewport.

--- client/components/MediumComponent.jsx
+++ client/components/MediumComponent.jsx
@@ -3,14 +3,17 @@ MediumComponent = React.createClass({
     var medium = this.props.medium;
     var type = medium.contentType.split("/")[0];
     var src = "/gridfs/media/" + medium.md5;
-    return {
-      image: <img className="mediumContent" src={src} />,
-      video: <video className="mediumContent" id={"video" + medium._id} src={src} controls>
+    var medium = {
+      image: <img className="medium" src={src} />,
+      video: <video className="medium" id={"video" + medium._id} src={src} controls>
         <source src={src} type={medium.contentType} />
       </video>,
-      audio: <audio className="mediumContent" id={"audio" + medium._id} src={src} controls>
+      audio: <audio className="medium" id={"audio" + medium._id} src={src} controls>
         <source src={src} type={medium.contentType} />
       </audio>
     }[type];
+    return <div className="mediumContainer">
+      {medium}
+    </div>;
   }
 });

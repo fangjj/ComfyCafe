1dc03f226a09b41a44e00b659b661b29e30ceafb
Teruko <elvesstolemybike@gmail.com>
2016-02-12 01:31:12 -0500

    Added post edit form with lots of code duplication.

--- client/components/Post/PostModifyFAB.jsx
+++ client/components/Post/PostModifyFAB.jsx
@@ -1,13 +1,31 @@
+let {
+  FloatingActionButton,
+  FontIcon
+} = mui;
+
 PostModifyFAB = React.createClass({
+  getInitialState() {
+    return {
+      showForm: false
+    };
+  },
+  showPostForm() {
+    this.setState({showForm: true});
+  },
+  destroyPostForm() {
+    this.setState({showForm: false});
+  },
+  renderPostForm() {
+    if (this.state.showForm) {
+      return <PostUpdateFormComponent post={this.props.post} destroy={this.destroyPostForm} />;
+    }
+  },
   render() {
     return <div id="fabModify" className="fixed-action-btn">
-      <a className="btn-floating btn-large">
-        <i className="large material-icons">mode_edit</i>
-      </a>
-      <ul>
-        <PostDeleteInnerFAB post={this.props.post} />
-        <PostRerollInnerFAB post={this.props.post} />
-      </ul>
+      <FloatingActionButton secondary={true} onClick={this.showPostForm}>
+        <FontIcon className="material-icons">edit</FontIcon>
+      </FloatingActionButton>
+      {this.renderPostForm()}
     </div>;
   }
 });
---
+++ client/components/Post/PostUpdateFormComponent.jsx
@@ -0,0 +1,87 @@
+let {
+  Dialog,
+  FlatButton,
+  RaisedButton,
+  TextField,
+  SelectField,
+  MenuItem
+} = mui;
+
+PostUpdateFormComponent = React.createClass({
+  getInitialState() {
+    return {
+      open: true,
+      visibility: this.props.post.visibility,
+      description: this.props.post.description,
+      tags: this.props.post.tags.text
+    };
+  },
+  handleOpen() {
+    this.setState({open: true});
+  },
+  handleClose() {
+    this.setState({open: false});
+  },
+  handleVisibility(event, index, value) {
+    this.setState({visibility: value});
+  },
+  handleDescription(event) {
+    this.setState({description: event.target.value});
+  },
+  handleTags(event) {
+    this.setState({tags: event.target.value});
+  },
+  handleSubmit() {
+    Meteor.call("updatePost", this.props.post._id, {
+      visibility: this.state.visibility,
+      description: this.state.description,
+      tags: this.state.tags
+    }, (err) => {
+      this.props.destroy();
+    });
+  },
+  render() {
+    const actions = [
+      <FlatButton
+        label="Cancel"
+        secondary={true}
+        onTouchTap={this.handleClose}
+      />,
+      <FlatButton
+        label="Submit"
+        primary={true}
+        onTouchTap={this.handleSubmit}
+      />,
+    ];
+
+    return <Dialog title="Edit Post" actions={actions} modal={true} open={this.state.open}>
+      <form>
+        <div>
+          <SelectField value={this.state.visibility} onChange={this.handleVisibility}>
+            <MenuItem value="public" primaryText="Public" />
+            <MenuItem value="friends" primaryText="Friends" />
+            <MenuItem value="unlisted" primaryText="Unlisted" />
+          </SelectField>
+        </div>
+        <br />
+        <TextField
+          hintText="Description"
+          defaultValue={this.state.description}
+          floatingLabelText="Description"
+          multiline={true}
+          rows={4}
+          onChange={this.handleDescription}
+        />
+        <br />
+        <TextField
+          hintText="Tags"
+          defaultValue={this.state.tags}
+          floatingLabelText="Tags"
+          multiline={true}
+          rows={4}
+          onChange={this.handleTags}
+        />
+      </form>
+    </Dialog>;
+  }
+});

35218f67e63c37c0b16d3f150ca7a5dd5d3f90df
Teruko <elvesstolemybike@gmail.com>
2016-02-12 00:32:17 -0500

    Only inject tap plugin once.

--- client/client.js
+++ client/client.js
@@ -1,7 +1,10 @@
+Meteor.startup(function(){
+  injectTapEventPlugin();  
+});
+
 Tracker.autorun(function () {
   document.title = Session.get("pageTitle") || "TeruImages";
   Meteor.subscribe("media", Meteor.userId());
   Meteor.subscribe("jobs", Meteor.userId());
   Cookie.set("X-Auth-Token", Accounts._storedLoginToken());
-  injectTapEventPlugin();
 });

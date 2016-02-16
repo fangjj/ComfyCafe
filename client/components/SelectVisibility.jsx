22979ede8563cb8a495f2dff537e8d579fa2a687
Teruko <elvesstolemybike@gmail.com>
2016-02-16 03:23:35 -0500

    Added label to visibility selector.

--- client/components/SelectVisibility.jsx
+++ client/components/SelectVisibility.jsx
@@ -10,6 +10,8 @@ SelectVisibility = React.createClass({
         value={this.props.visibility}
         onChange={this.props.onChange}
         fullWidth={true}
+        floatingLabelText="Visibility"
+        floatingLabelStyle={{fontSize: "20px"}}
       >
         <MenuItem value="public" primaryText="Public" />
         <MenuItem value="friends" primaryText="Friends" />

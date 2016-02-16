bc9c8c5ed079e091f22326f5b47646d0e9de14a7
Teruko <elvesstolemybike@gmail.com>
2016-02-16 00:50:39 -0500

    Set tag button to dummy view so people don't complain to me when it doesn't do anything.

--- client/components/TopBar/TopBarTagButton.jsx
+++ client/components/TopBar/TopBarTagButton.jsx
@@ -1,7 +1,7 @@
 TopBarTagButton = React.createClass({
   render() {
     return <li>
-      <a href={""} className="waves-effect waves-teal">
+      <a href={"/pizza"} className="waves-effect waves-teal">
         <i className="material-icons left">style</i>
         <span className="hide-on-med-and-down">Tags</span>
       </a>

48a162063bdb4add1765b739a27ee75624b7d001
Teruko <elvesstolemybike@gmail.com>
2016-02-19 07:06:40 -0500

    UserLink uses vanilla tooltip instead of popover, since the latter was too finicky.

--- client/components/User/UserLink.jsx
+++ client/components/User/UserLink.jsx
@@ -19,19 +19,9 @@ UserLink = React.createClass({
     var user = this.props.user;
     var path = FlowRouter.path("profile", {username: user.username});
     return <div style={{display: "inline"}}>
-      <a
-        href={path}
-        onMouseEnter={this.handleTouchTap}
-        onMouseLeave={this.handleRequestClose}
-      >
+      <a href={path} title={user.profile.blurb}>
         {user.profile.displayName || user.username}
       </a>
-      <UserPopover
-        user={this.props.user}
-        open={this.state.open}
-        anchorEl={this.state.anchorEl}
-        onRequestClose={this.handleRequestClose}
-      />
     </div>;
   }
 });

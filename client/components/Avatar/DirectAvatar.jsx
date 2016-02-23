df63417a6ccd9d85a1d1b115d60bacf0a83fa3bb
Teruko <elvesstolemybike@gmail.com>
2016-02-23 14:15:37 -0500

    Fixed handling for absent avatar.

--- client/components/Avatar/DirectAvatar.jsx
+++ client/components/Avatar/DirectAvatar.jsx
@@ -3,18 +3,20 @@ DirectAvatar = React.createClass({
     const user = this.props.user;
     var hasAvatars = Boolean(user.avatars);
     if (hasAvatars) {
-      var md5 = (user.avatars[this.props.size] || user.avatars.fullsize).md5;
-  		return <img
-        className={"avatar " + this.props.size}
-        src={"/gridfs/media/" + md5}
-        title={user.profile.displayName || user.username}
-      />
-    } else {
-		  return <img
-        className={"avatar " + this.props.size}
-        src={"/gridfs/media/djent/" + user._id}
-        title={user.profile.displayName || user.username}
-      />
+      const avatar = user.avatars[this.props.size] || user.avatars.fullsize;
+      if (avatar) {
+        return <img
+          className={"avatar " + this.props.size}
+          src={"/gridfs/media/" + avatar.md5}
+          title={user.profile.displayName || user.username}
+        />
+      }
     }
+
+    return <img
+      className={"avatar " + this.props.size}
+      src={"/gridfs/media/djent/" + user._id}
+      title={user.profile.displayName || user.username}
+    />
   }
 });

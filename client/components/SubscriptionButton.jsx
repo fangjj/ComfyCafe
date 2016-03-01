7e91eae44703ca82b0093938d0f24c3319a0480f
Teruko <elvesstolemybike@gmail.com>
2016-03-01 18:03:34 -0500

    Used ToggleButton for SubscriptionButton.

--- client/components/SubscriptionButton.jsx
+++ client/components/SubscriptionButton.jsx
@@ -1,56 +1,21 @@
-let {
-  RaisedButton,
-  FontIcon
-} = mui;
-
 SubscriptionButton = React.createClass({
-  style: {
-    width: 170
-  },
-  getInitialState() {
-    return {
-      notHover: true
-    };
-  },
   toggleSubscription(event) {
     Meteor.call("toggleSubscription", this.props.owner._id);
   },
-  toggleHover() {
-    this.setState({notHover: ! this.state.notHover});
-  },
   render() {
-    var subscribed = this.props.currentUser
+    const subscribed = this.props.currentUser
       && _.contains(this.props.currentUser.subscriptions, this.props.owner._id);
-    if (! subscribed) {
-      return <RaisedButton
-        className="toggleSubscription"
-        label="Subscribe"
-        labelStyle={{fontSize: "18px"}}
-        secondary={true}
-        icon={<FontIcon className="material-icons">add_box</FontIcon>}
-        style={this.style}
-        onTouchTap={this.toggleSubscription}
-      />;
-    } else {
-      var label;
-      var icon;
-      if (this.state.notHover) {
-        label = "Subscribed";
-        icon = "check_box";
-      } else {
-        label = "Unsubscribe";
-        icon = "indeterminate_check_box";
-      }
-      return <RaisedButton
-        className="toggleSubscription grey darken-2"
-        label={label}
-        labelStyle={{fontSize: "18px"}}
-        icon={<FontIcon className="material-icons">{icon}</FontIcon>}
-        style={this.style}
-        onTouchTap={this.toggleSubscription}
-        onMouseEnter={this.toggleHover}
-        onMouseLeave={this.toggleHover}
-      />;
-    }
+    return <ToggleButton
+      active={subscribed}
+      activate={this.toggleSubscription}
+      deactivate={this.toggleSubscription}
+      labelActivate="Subscribe"
+      iconActivate="add_box"
+      labelActivated="Subscribed"
+      iconActivated="check_box"
+      labelDeactivate="Unsubscribe"
+      iconDeactivate="indeterminate_check_box"
+      width={170}
+    />;
   }
 });

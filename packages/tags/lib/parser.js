bd58dacd5bd1267aeeae40d3c7eef8eb9d074ee0
Teruko <elvesstolemybike@gmail.com>
2016-03-08 09:53:26 -0500

    Top-level without.

--- packages/tags/lib/parser.js
+++ packages/tags/lib/parser.js
@@ -1,6 +1,60 @@
 sampleTagStr = "nia-teppelin: young, short multicolored hair, cat ears;"
 	+ "yoko-littner: flame bikini, pink stockings, long red hair, without gun";
 
+function parseLonely(parsed, kv) {
+	var tokens = kv[0].split(/\s+/);
+	if (tokens.length === 1) {
+		parsed.subjects[kv[0]] = {};
+	} else if (_.contains(["not", "without"], tokens[0])) {
+		parsed.without[tokens[1]] = {};
+	}
+}
+
+function parseDescriptors(parsed, kv) {
+	var label;
+	var withoutMode = false;
+	var topTokens = kv[0].split(/\s+/);
+	if (topTokens.length === 1) {
+		label = kv[0];
+	} else if (_.contains(["not", "without"], topTokens[0])) {
+		label = topTokens[1];
+		withoutMode = true;
+	}
+
+	var sInner = {}, wInner = {};
+
+	var descriptors = kv[1].split(/\s*,\s*/);
+
+	for (di in descriptors) {
+		var tokens = descriptors[di].split(/\s+/);
+
+		var target;
+		if (tokens[0] !== "without") {
+			target = sInner;
+		} else {
+			if (! withoutMode) {
+				target = wInner;
+			} else {
+				target = sInner;
+			}
+			tokens.shift();
+		}
+
+		target[tokens.pop()] = tokens;
+	}
+
+	if (! _.isEmpty(sInner)) {
+		if (! withoutMode) {
+			parsed.subjects[label] = sInner;
+		} else {
+			parsed.without[label] = sInner;
+		}
+	}
+	if (! _.isEmpty(wInner)) {
+		parsed.without[label] = wInner;
+	}
+}
+
 parseTagStr = function (tagStr) {
 	var parsed = {
 		authors: [],
@@ -15,44 +69,21 @@ parseTagStr = function (tagStr) {
 		var topToken = toplevel[ti];
 
 		if (topToken.substr(0, 3) === "by ") {
-			parsed.authors.push(topToken.substr(3));
+			parsed.authors.push(topToken.substr(3).trim());
 			continue;
 		}
 
 		if (topToken.substr(0, 7) === "not by ") {
-			parsed.notAuthors.push(topToken.substr(7));
+			parsed.notAuthors.push(topToken.substr(7).trim());
 			continue;
 		}
 
 		var kv = topToken.split(/\s*:\s*/);
 
 		if (kv.length === 1) {
-			parsed.subjects[kv[0]] = {};
+			parseLonely(parsed, kv);
 		} else {
-			var sInner = {}, wInner = {};
-
-			var descriptors = kv[1].split(/\s*,\s*/);
-
-			for (di in descriptors) {
-				var tokens = descriptors[di].split(/\s+/);
-
-				var target;
-				if (tokens[0] !== "without") {
-					target = sInner;
-				} else {
-					target = wInner;
-					tokens.shift();
-				}
-
-				target[tokens.pop()] = tokens;
-			}
-
-			if (! _.isEmpty(sInner)) {
-				parsed.subjects[kv[0]] = sInner;
-			}
-			if (! _.isEmpty(wInner)) {
-				parsed.without[kv[0]] = wInner;
-			}
+			parseDescriptors(parsed, kv);
 		}
 	}
 

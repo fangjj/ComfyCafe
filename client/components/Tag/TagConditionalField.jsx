78ed3d439aca59d143207892e610401c1f46276d
Teruko <elvesstolemybike@gmail.com>
2016-03-15 12:15:18 -0400

    condimpl field population and editing actually works!

--- client/components/Tag/TagConditionalField.jsx
+++ client/components/Tag/TagConditionalField.jsx
@@ -5,29 +5,27 @@ let {
 TagConditionalField = React.createClass({
   getInitialState() {
     return {
-      cond: "",
+      cond: this.props.defaultCondition || "",
       impl: this.props.defaultImplications || ""
     };
-  },
-  componentWillMount() {
-    this.id = _.uniqueId();
   },
   handleCond(e) {
     this.setState({
       cond: e.target.value
     });
-    this.props.onChange(this.id, e.target.value, this.state.impl);
+    this.props.onChange(this.props.condImplId, e.target.value, this.state.impl);
   },
   handleImpl(value) {
     this.setState({
       impl: value
     });
-    this.props.onChange(this.id, this.state.cond, value);
+    this.props.onChange(this.props.condImplId, this.state.cond, value);
   },
   render() {
     return <li className="condImplicationField">
       <div className="condField">
         <TextField
+          defaultValue={this.props.defaultCondition}
           floatingLabelText="Condition"
           floatingLabelStyle={{fontSize: "20px"}}
           onChange={this.handleCond}

bb5dbfb7fd1a3a4a03387f786a06b09361010352
Teruko <elvesstolemybike@gmail.com>
2016-03-19 00:42:13 -0400

    Attempt to use mui DropDownMenu for suggestions.

--- client/components/Suggestions.jsx
+++ client/components/Suggestions.jsx
@@ -1,26 +1,43 @@
 let {
-  Menu,
+  DropDownMenu,
   MenuItem
 } = mui;
 
 Suggestions = React.createClass({
+  lookup: {},
+  getInitialState() {
+    return {
+      selected: undefined
+    };
+  },
+  handleSelect(event, index, value) {
+    this.setState({
+      selected: value
+    });
+    this.props.onSelect(this.lookup[value]);
+  },
   renderSuggestions() {
     if (this.props.suggestions && this.props.suggestions.length) {
       return this.props.suggestions.map((s) => {
+        this.lookup[s._id] = s;
         return <MenuItem
           key={s._id}
-          onTouchTap={() => {
-            this.props.onSelect(s);
-          }}
-        >{s.name}</MenuItem>;
+          value={s._id}
+          primaryText={s.name}
+        />;
       });
     }
   },
   renderInner() {
     if (this.props.suggestions && this.props.suggestions.length) {
-      return <Menu className="suggestions list">
+      return <DropDownMenu
+        className="suggestions"
+        value={this.state.selected}
+        openImmediately={true}
+        onChange={this.handleSelect}
+      >
         {this.renderSuggestions()}
-      </Menu>;
+      </DropDownMenu>;
     }
   },
   render() {

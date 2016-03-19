3bfc02229dacfd893e53d239ece258275c12eef5
Teruko <elvesstolemybike@gmail.com>
2016-03-19 00:57:49 -0400

    Go back to vanilla list but now with LinkButtons; some fancy keybinding will be required to get the desired UX.

--- client/components/Suggestions.jsx
+++ client/components/Suggestions.jsx
@@ -4,40 +4,20 @@ let {
 } = mui;
 
 Suggestions = React.createClass({
-  lookup: {},
-  getInitialState() {
-    return {
-      selected: undefined
-    };
-  },
-  handleSelect(event, index, value) {
-    this.setState({
-      selected: value
-    });
-    this.props.onSelect(this.lookup[value]);
-  },
   renderSuggestions() {
     if (this.props.suggestions && this.props.suggestions.length) {
       return this.props.suggestions.map((s) => {
-        this.lookup[s._id] = s;
-        return <MenuItem
-          key={s._id}
-          value={s._id}
-          primaryText={s.name}
-        />;
+        return <LinkButton key={s._id} primaryText={s.name} onTouchTap={() => {
+          this.props.onSelect(s);
+        }} />;
       });
     }
   },
   renderInner() {
     if (this.props.suggestions && this.props.suggestions.length) {
-      return <DropDownMenu
-        className="suggestions"
-        value={this.state.selected}
-        openImmediately={true}
-        onChange={this.handleSelect}
-      >
+      return <ul className="suggestions list">
         {this.renderSuggestions()}
-      </DropDownMenu>;
+      </ul>;
     }
   },
   render() {

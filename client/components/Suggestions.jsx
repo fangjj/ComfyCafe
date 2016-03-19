643f9145869a46812e91cace870bdd7eebbd37ba
Teruko <elvesstolemybike@gmail.com>
2016-03-19 14:56:30 -0400

    Fancy keypress handling in suggestion list.

--- client/components/Suggestions.jsx
+++ client/components/Suggestions.jsx
@@ -4,12 +4,106 @@ let {
 } = mui;
 
 Suggestions = React.createClass({
+  getInitialState() {
+    return {
+      selected: undefined
+    };
+  },
+  lookup: {},
+  keyHandler(e) {
+    if (! this.state.selected) {
+      return;
+    }
+
+    const keys = {
+      ENTER: 13,
+      //LEFT: 37,
+      UP: 38,
+      //RIGHT: 39,
+      DOWN: 40
+    };
+
+    const keyDown = e.which;
+    if (! _.includes(_.values(keys), keyDown)) {
+      return;
+    }
+
+    e.preventDefault();
+
+    const currentIndex = () => {
+      if (this.state.selected) {
+        return this.lookup[this.state.selected];
+      } return 0;
+    }();
+    const lookupLength = _.keys(this.lookup).length;
+    const canGoDown = currentIndex < lookupLength;
+    const canGoUp = currentIndex !== 0 && lookupLength > 1;
+
+    const actionMap = {};
+    actionMap[keys.DOWN] = () => {
+      if (canGoDown) {
+        // Reckless reduction!
+        return _.reduce(
+          this.lookup,
+          (result, value, key) => {
+            if (value === currentIndex + 1) {
+              return key;
+            } return result;
+          },
+          this.state.selected
+        );
+      }
+    };
+    actionMap[keys.UP] = () => {
+      if (canGoUp) {
+        return _.reduce(
+          this.lookup,
+          (result, value, key) => {
+            if (value === currentIndex - 1) {
+              return key;
+            } return result;
+          },
+          this.state.selected
+        );
+      }
+    };
+
+    const selection = actionMap[keyDown]();
+    if (selection) {
+      this.setState({
+        selected: selection
+      });
+    }
+  },
+  componentDidMount() {
+    window.addEventListener("keydown", this.keyHandler);
+  },
+  componentWillUnmount() {
+    window.removeEventListener("keydown", this.keyHandler);
+  },
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.suggestions && nextProps.suggestions !== this.props.suggestions) {
+      _.each(nextProps.suggestions, (value, idx) => {
+        this.lookup[value._id] = idx;
+        if (idx === 0) {
+          this.setState({
+            selected: nextProps.suggestions[0]._id
+          });
+        }
+      });
+    }
+  },
   renderSuggestions() {
     if (this.props.suggestions && this.props.suggestions.length) {
-      return this.props.suggestions.map((s) => {
-        return <LinkButton key={s._id} primaryText={s.name} onTouchTap={() => {
-          this.props.onSelect(s);
-        }} />;
+      return this.props.suggestions.map((s, idx) => {
+        return <li
+          className={this.state.selected === s._id ? "selected" : ""}
+          key={s._id}
+        >
+          <LinkButton primaryText={s.name} onTouchTap={() => {
+            this.props.onSelect(s);
+          }} />
+        </li>;
       });
     }
   },

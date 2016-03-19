3a163cabbc9f38f90f4a6ca65ab7275fda31754b
Teruko <elvesstolemybike@gmail.com>
2016-03-19 00:12:29 -0400

    Use a mui Menu for suggestions.

--- client/components/Suggestions.jsx
+++ client/components/Suggestions.jsx
@@ -1,19 +1,31 @@
+let {
+  Menu,
+  MenuItem
+} = mui;
+
 Suggestions = React.createClass({
   renderSuggestions() {
     if (this.props.suggestions && this.props.suggestions.length) {
       return this.props.suggestions.map((s) => {
-        return <li
+        return <MenuItem
           key={s._id}
           onTouchTap={() => {
             this.props.onSelect(s);
           }}
-        >{s.name}</li>;
+        >{s.name}</MenuItem>;
       });
     }
   },
+  renderInner() {
+    if (this.props.suggestions && this.props.suggestions.length) {
+      return <Menu className="suggestions list">
+        {this.renderSuggestions()}
+      </Menu>;
+    }
+  },
   render() {
-    return <ul className="suggestions list">
-      {this.renderSuggestions()}
-    </ul>;
+    return <div className="suggestionContainer">
+      {this.renderInner()}
+    </div>;
   }
 });

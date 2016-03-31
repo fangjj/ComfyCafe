64130380e96b0111651e8e09d1b917706f751dc8
Teruko <elvesstolemybike@gmail.com>
2016-03-31 05:14:00 -0400

    Finally figured out how to correctly set muiTheme userAgent, so SSR no longer has garbage performance!

--- imports/api/common/renderView.js
+++ imports/api/common/renderView.js
@@ -4,6 +4,10 @@ export default function (view) {
   if (Meteor.isClient) {
     Session.set("previousPath", Session.get("currentPath") || "/");
     Session.set("currentPath", FlowRouter.current().path);
+  } else {
+    global.navigator = {
+      userAgent: FlowRouter.current()._serverRequest.headers["user-agent"] || "all"
+    };
   }
   mount(view.layout, view.content);
 };

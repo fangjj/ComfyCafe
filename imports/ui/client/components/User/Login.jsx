822f1e92aea48c059ddb70cec92d955520f1d1b9
Teruko <elvesstolemybike@gmail.com>
2016-04-02 00:23:33 -0400

    Cute error thingies on login form.

--- imports/ui/client/components/User/Login.jsx
+++ imports/ui/client/components/User/Login.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import goBack from "/imports/ui/client/utils/goBack"
+import Colors from "/imports/ui/client/utils/colors"
 import Content from "/imports/ui/client/components/Content";
 import Actions from "/imports/ui/client/components/Actions";
 import CancelButton from "/imports/ui/client/components/Button/CancelButton";
@@ -31,8 +32,28 @@ export default React.createClass({
   },
   handleSubmit(e) {
     e.preventDefault();
-    Meteor.loginWithPassword(this.state.username, this.state.password, () => {
-      goBack();
+    Meteor.loginWithPassword(this.state.username, this.state.password, (err) => {
+      if (err) {
+        const errorMap = {
+          "User not found": () => {
+            this.setState({
+              usernameError: "You don't exist! Perhaps you'd like to register?"
+            });
+          },
+          "Incorrect password": () => {
+            this.setState({
+              passwordError: "That isn't your password, dingus!"
+            });
+          }
+        };
+        if (_.has(errorMap, err.reason)) {
+          errorMap[err.reason]();
+        } else {
+          prettyPrint(err);
+        }
+      } else {
+        goBack();
+      }
     });
   },
   render() {
@@ -49,6 +70,11 @@ export default React.createClass({
           name="username"
           floatingLabelText="Username"
           floatingLabelStyle={{fontSize: "20px"}}
+          errorText={this.state.usernameError}
+          errorStyle={{
+            fontSize: "16px",
+            color: Colors.poisonPink
+          }}
           fullWidth={true}
           onChange={this.handleUsername}
         />
@@ -57,6 +83,11 @@ export default React.createClass({
           type="password"
           floatingLabelText="Password"
           floatingLabelStyle={{fontSize: "20px"}}
+          errorText={this.state.passwordError}
+          errorStyle={{
+            fontSize: "16px",
+            color: Colors.poisonPink
+          }}
           fullWidth={true}
           onChange={this.handlePassword}
         />

5d22256e66ca61a36d4e46648720aad1e3dbbb4b
Teruko <elvesstolemybike@gmail.com>
2016-04-09 09:56:29 -0400

    Implemented button hover color thingy.

--- imports/ui/client/components/Button/Button.jsx
+++ imports/ui/client/components/Button/Button.jsx
@@ -1,28 +1,54 @@
 import _ from "lodash";
 import React from "react";
 import RaisedButton from "material-ui/lib/raised-button";
+import tinycolor from "tinycolor2";
 
+import Colors from "/imports/ui/client/utils/colors";
 import classConcat from "/imports/ui/client/utils/classConcat";
 import Icon from "/imports/ui/client/components/Daikon/Icon";
 
-export default (props) => {
-  const {
-    className,
-    iconName,
-    label,
-    downStyle,
-    width,
-    ...leftoverProps
-  } = props;
+export default React.createClass({
+  getInitialState() {
+    return {
+      hover: false
+    };
+  },
+  hover() {
+    this.setState({ hover: true });
+  },
+  unhover() {
+    this.setState({ hover: false });
+  },
+  render() {
+    const {
+      backgroundColor,
+      primary,
+      className,
+      iconName,
+      label,
+      downStyle,
+      width,
+      ...leftoverProps
+    } = this.props;
 
-  const classes = classConcat("button", className);
-  const style = _.defaults({ width: width }, downStyle);
-  return <RaisedButton
-    className={classes}
-    label={label}
-    labelStyle={{ fontSize: "18px" }}
-    icon={<Icon>{iconName}</Icon>}
-    style={style}
-    {...leftoverProps}
-  />;
-};
+    const classes = classConcat("button", className);
+    const style = _.defaults({ width: width }, downStyle);
+
+    let color = backgroundColor || Colors.friendlyTeal;
+    if (this.state.hover) {
+      color = tinycolor(color).lighten(6).toHexString();
+    }
+
+    return <RaisedButton
+      backgroundColor={color}
+      className={classes}
+      label={label}
+      labelStyle={{ fontSize: "18px" }}
+      icon={<Icon>{iconName}</Icon>}
+      style={style}
+      onMouseEnter={this.hover}
+      onMouseLeave={this.unhover}
+      {...leftoverProps}
+    />;
+  }
+});
--- imports/ui/client/utils/colors.js
+++ imports/ui/client/utils/colors.js
@@ -1,7 +1,6 @@
-const Colors = {
+export default {
+  friendlyTeal: "#00897B",
   reassuringGray: "#616161",
   scaryCherry: "#C93838",
   poisonPink: "#FF7676"
 };
-
-export default Colors;

7bb0e559a70e4cc4fc08549c81287294ccee78fe
Teruko <elvesstolemybike@gmail.com>
2016-04-15 15:20:31 -0400

    Use ActionWell for topics.

--- imports/ui/client/components/Chat/Topic.jsx
+++ imports/ui/client/components/Chat/Topic.jsx
@@ -1,15 +1,25 @@
 import React from "react";
 
 import Topics from "/imports/api/topics/collection";
+import "/imports/api/topics/methods";
 import setTitle from "/imports/api/common/setTitle";
 
-import TopicMoreMenu from "./TopicMoreMenu";
 import MessageList from "./MessageList";
+import TopicUpdateForm from "./TopicUpdateForm";
 import WatchButton from "/imports/ui/client/components/Button/WatchButton";
+import SubmitButton from "/imports/ui/client/components/Button/SubmitButton";
+import DangerButton from "/imports/ui/client/components/Button/DangerButton";
+import ButtonGroup from "/imports/ui/client/components/Button/ButtonGroup";
+import ActionWell from "/imports/ui/client/components/ActionWell";
+import PrivacyIcon from "/imports/ui/client/components/Daikon/PrivacyIcon";
+import Moment from "/imports/ui/client/components/Moment";
 import DenseLoadingSpinner from "/imports/ui/client/components/Spinner/DenseLoadingSpinner";
 
 export default React.createClass({
   mixins: [ReactMeteorData],
+  getInitialState() {
+    return { showForm: false };
+  },
   getMeteorData() {
     var id = FlowRouter.getParam("topicId");
     var handle = Meteor.subscribe("topic", id);
@@ -27,17 +37,48 @@ export default React.createClass({
     }
     setTitle(pre + body);
   },
-  renderMoreMenu() {
-    var isOwner = this.data.currentUser
+  showTopicForm() {
+    this.setState({ showForm: true });
+  },
+  hideTopicForm() {
+    this.setState({ showForm: false });
+  },
+  delete() {
+    const path = FlowRouter.path("room", { roomId: this.data.topic.room._id });
+    Meteor.call("deleteTopic", this.data.topic._id, () => {
+      FlowRouter.go(path);
+    });
+  },
+  renderEditButtons() {
+    const isOwner = this.data.currentUser
       && this.data.currentUser._id === this.data.topic.owner._id;
     if (isOwner) {
-      return <div className="topRight">
-        <TopicMoreMenu
-          topic={this.data.topic}
-          currentUser={this.data.currentUser}
-          redirect={true}
+      return <ButtonGroup>
+        <SubmitButton
+          label="Edit"
+          iconName="edit"
+          onTouchTap={this.showTopicForm}
         />
-      </div>;
+        <DangerButton
+          label="Delete"
+          iconName="close"
+          subtle={true}
+          onTouchTap={this.delete}
+        />
+        <TopicUpdateForm
+          topic={this.data.topic}
+          handleClose={this.hideTopicForm}
+          open={this.state.showForm}
+        />
+      </ButtonGroup>;
+    } else {
+      return <ButtonGroup>
+        <DangerButton
+          label="Flag"
+          iconName="flag"
+          subtle={true}
+        />
+      </ButtonGroup>;
     }
   },
   render() {
@@ -45,19 +86,23 @@ export default React.createClass({
       return <DenseLoadingSpinner />;
     }
 
-    var topic = this.data.topic;
+    const topic = this.data.topic;
 
-    var room = topic.room;
-    var roomUrl = FlowRouter.path("room", {roomId: room._id});
+    const room = topic.room;
+    const roomUrl = FlowRouter.path("room", {roomId: room._id});
 
     return <section className="msgList">
       <header>
-        {this.renderMoreMenu()}
         <h2>{topic.name}</h2>
-        <WatchButton
-          topic={this.data.topic}
-          currentUser={this.data.currentUser}
-        />
+        <ActionWell>
+          {this.renderEditButtons()}
+          <ButtonGroup>
+            <WatchButton
+              topic={this.data.topic}
+              currentUser={this.data.currentUser}
+            />
+          </ButtonGroup>
+        </ActionWell>
       </header>
       <MessageList
         topic={this.data.topic}

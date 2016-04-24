c803c6dcfb2ceaf823490fd4e6ca8fe78c37ba38
Teruko <elvesstolemybike@gmail.com>
2016-04-24 17:43:55 -0400

    Actually implemented section prop for InfoBox.

--- imports/ui/client/components/InfoBox.jsx
+++ imports/ui/client/components/InfoBox.jsx
@@ -3,9 +3,15 @@ import React from "react";
 import classConcat from "/imports/ui/client/utils/classConcat";
 
 export default (props) => {
-  const { className, children, ...leftoverProps } = props;
+  const { className, section, children, ...leftoverProps } = props;
   const classes = classConcat("infoBox", className);
-  return <div className={classes} {...leftoverProps}>
-    {children}
-  </div>;
+  if (section) {
+    return <section className={classes} {...leftoverProps}>
+      {children}
+    </section>;
+  } else {
+    return <div className={classes} {...leftoverProps}>
+      {children}
+    </div>;
+  }
 };

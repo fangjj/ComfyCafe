8aa3ac2c307f4e1734062286a934f37b8479ede7
Teruko <elvesstolemybike@gmail.com>
2016-04-25 16:19:16 -0400

    Removed UserPopover, since hovering doesn't work so well on mobile.

--- imports/ui/client/components/User/UserLink.jsx
+++ imports/ui/client/components/User/UserLink.jsx
@@ -1,39 +1,12 @@
 import _ from "lodash";
 import React from "react";
 
-import UserPopover from "/imports/ui/client/components/User/UserPopover";
-
-export default React.createClass({
-  getInitialState() {
-    return { open: false }
-  },
-  showPopover(e) {
-    const $link = $(this.refs.link);
-    const anchor = $link.offset().left;
-    this.setState({
-      open: true,
-      anchor: anchor,
-    });
-  },
-  hidePopover() {
-    this.setState({ open: false });
-  },
-  renderPopover() {
-    return <UserPopover
-      user={this.props.user}
-      anchor={this.state.anchor}
-      open={this.state.open}
-      onClose={this.hidePopover}
-    />;
-  },
-  render() {
-    const user = this.props.user;
-    const path = FlowRouter.path("profile", {username: user.username});
-    return <span className="userLink">
-      <a ref="link" href={path} onMouseEnter={this.showPopover}>
-        {user.profile.displayName || user.username}
-      </a>
-      {this.renderPopover()}
-    </span>;
-  }
-});
+export default (props) => {
+  const user = props.user;
+  const path = FlowRouter.path("profile", {username: user.username});
+  return <span className="userLink">
+    <a href={path} title={user.profile.blurb}>
+      {user.profile.displayName || user.username}
+    </a>
+  </span>;
+};

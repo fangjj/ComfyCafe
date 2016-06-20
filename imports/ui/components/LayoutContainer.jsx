81c7bba8a9f3293352b11412ab9100744cb2cbd0
Scott Pleb <elvesstolemybike@gmail.com>
2016-06-19 22:29:04 -0400

    Set base meta tags in LayoutContainer, since hardcoding them into base.html caused them to override subsequent meta tags.

--- imports/ui/components/LayoutContainer.jsx
+++ imports/ui/components/LayoutContainer.jsx
@@ -1,6 +1,7 @@
 import { Meteor } from "meteor/meteor";
 import { createContainer } from "meteor/react-meteor-data";
 
+import metaBuilder from "/imports/ui/utils/metaBuilder";
 import Layout from "./Layout";
 
 let doneCallback;
@@ -9,6 +10,11 @@ if (Meteor.isClient) {
 }
 
 export default createContainer(({ params }) => {
+  metaBuilder({
+    title: "ComfyCafé",
+    description: "The image site for cool kids."
+  });
+
   return {
     currentUser: Meteor.user(),
     passwordResetToken: Session.get("passwordResetToken"),

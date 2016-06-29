afbc286b146604cc606d7b6028798ca3d1b745c3
Scott Pleb <elvesstolemybike@gmail.com>
2016-06-29 00:04:40 -0400

    Fixed rsync flags and added separate '--no-sync' option to deploy script.

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,9 @@ if [ "$1" != "--no-build" ]
   then
     mkdir -p ../meteor-bundles/ComfyCafé
     meteor build --directory ../meteor-bundles/ComfyCafé
-    rsync -azP 'ssh -p 22 -i ~/.ssh/id_rsa' --delete ../meteor-bundles/ComfyCafé/bundle root@git.comfy.cafe:/srv/ComfyCafé
+fi
+if [ "$1" != "--no-sync" ]
+  then
+    rsync -azPAXvH -e 'ssh -p 22 -i ~/.ssh/id_rsa' --delete ../meteor-bundles/ComfyCafé/bundle root@git.comfy.cafe:/srv/ComfyCafé
 fi
 ansible-playbook ansible/site.yml

2688371f61a92ae167e129439bde64f0809a5be4
Scott Pleb <scottpleb@comfy.cafe>
2016-08-25 15:05:40 -0400

    Added missing lodash imports everywhere.

--- imports/api/users/injectOwner.js
+++ imports/api/users/injectOwner.js
@@ -1,3 +1,4 @@
+import _ from "lodash";
 function injectOwner(doc, user) {
   if (! user) { user = Meteor.user(); }
   doc.owner = _.pick(user, [ "_id", "username", "normalizedUsername", "profile" ]);
